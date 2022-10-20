b9c849a90ba506245c7ee77c7fdd72fbd381093e
RuoningJin <ronjin2013@hotmail.com>
2022-10-19 22:00:36 -0400

    adding function to check if two objects are equal with possible arrays as the values

---
+++ eqObjects.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  let output;
+  actual === expected ? (output = `😁😁😁Assertion Passed: ${actual} === ${expected}`) : (output = `🥲Assertion Not Passed: ${actual} !== ${expected}`);
+  console.log(output);
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (Array.isArray(object1[key])) {
+      for (let i = 0; i < object1[key].length; i++) {
+        if (object1[key][i] !== object2[key][i] || object1[key].length !== object2[key].length) {
+          return false;
+        }
+      }
+    } else if (object2[key] !== object1[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
