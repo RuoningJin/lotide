ef7ba643f86512ad1e436c5bf5739b41b5c7449a
RuoningJin <ronjin2013@hotmail.com>
2022-10-20 18:03:49 -0400

    a map function added

---
+++ map.js
@@ -0,0 +1,21 @@
+const assertArraysEqual = function(actual, expected) {
+  let output = true;
+  for (let i = 0; i < expected.length; i++) {
+    if (expected[i] !== actual[i] || expected.length !== actual.length) {
+      output = false; break;
+    }
+  }
+  output === true ? console.log(`😁😁😁Assertion Passed: ${actual} === ${expected}`) : console.log(`🥲Assertion Not Passed: ${actual} !== ${expected}`);
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word);
+assertArraysEqual(results1, ["ground", "control", "to", "major", "tom"])
