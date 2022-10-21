bf2bd42b28824d495021537a20bfc1270aa0f2e2
RuoningJin <ronjin2013@hotmail.com>
2022-10-20 21:20:12 -0400

    implement of findkeys

---
+++ findKey.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  let output;
+  actual === expected ? (output = `😁😁😁Assertion Passed: ${actual} === ${expected}`) : (output = `🥲Assertion Not Passed: ${actual} !== ${expected}`);
+  console.log(output);
+};
+
+const findKey = (names, backcall) => {
+  for (const name in names) {
+    if (backcall(names[name])) {
+
+      return name;
+    }
+  }
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
