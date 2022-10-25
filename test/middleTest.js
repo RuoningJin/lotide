fa4f713be75723b1dbeb15f2c4d2f9566b6606cd
RuoningJin <ronjin2013@hotmail.com>
2022-10-24 21:32:36 -0400

    modified middleTest and tailTest so that deepEqual is used

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,15 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
-const eqArray = require('../eqArrays');
 
 describe("#middle", () => {
   
   it("returns an empty array if the length of input array is less than 3", () => {
-    assert.isTrue(eqArray(middle([1, 2]), []));
+    assert.deepEqual(middle([1, 2]), []);
   });
   it("returns the middle item if the length of the input array is odd and greater than 2", () => {
-    assert.isTrue(eqArray(middle([1, 2, 3]), [2]));
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
   it("returns the middle two items if the length of the input array is even and greater than 2", () => {
-    assert.isTrue(eqArray(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,12 @@
 const assert = require('chai').assert;
 const tail = require('../tail');
-const eqArray = require('../eqArrays');
 
 describe("#middle", () => {
   
   it("returns an empty array if the array is empty", () => {
-    assert.isTrue(eqArray(tail([]), []));
+    assert.deepEqual(tail([]), []);
   });
   it("returns the middle two items if the length of the input array is even and greater than 2", () => {
-    assert.isTrue(eqArray(tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6]));
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6]);
   });
 });
